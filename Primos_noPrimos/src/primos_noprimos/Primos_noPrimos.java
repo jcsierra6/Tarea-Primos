a080f4afb718c4af2a71c1d9a61a250fd229de5d
Juan Carlos Sierra <>
2016-10-24 18:26:24 +0200

    Tarea de los numeros primos.

---
+++ Primos_noPrimos/src/primos_noprimos/Primos_noPrimos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primos_noprimos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class Primos_noPrimos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void primos(){
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        int num = 0;
+        System.out.println("Escribe un numero:");
+        primos();
+    }
+    
+}
